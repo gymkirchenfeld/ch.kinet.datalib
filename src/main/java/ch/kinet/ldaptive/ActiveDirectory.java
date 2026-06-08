6577ef53c10e7e4bb94a168681f5680b5ee59ffb
Stefan Rothe <stefan.rothe@gymkirchenfeld.ch>
2026-06-08 13:42:05 +0200

    ActiveDirectory - Add information about account in exception.

--- src/main/java/ch/kinet/ldaptive/ActiveDirectory.java
+++ src/main/java/ch/kinet/ldaptive/ActiveDirectory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 - 2026 by Stefan Rothe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY); without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package ch.kinet.ldaptive;
 
 import java.time.Duration;
@@ -51,7 +67,7 @@ public class ActiveDirectory {
             }
         }
         catch (LdapException ex) {
-            throw new RuntimeException(ex);
+            throw new RuntimeException("Error while adding ad object " + object.toString() + ".", ex);
         }
     }
 
